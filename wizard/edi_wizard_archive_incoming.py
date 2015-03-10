1f93345d16b1e13d0de33e8231506eebc5443c92
Jan Vereecken <janvereecken@clubit.be>
2015-03-10 14:28:31 +0100

    allow archiving of documents in other states

--- wizard/edi_wizard_archive_incoming.py
+++ wizard/edi_wizard_archive_incoming.py
@@ -12,19 +12,18 @@ class clubit_tools_edi_wizard_archive_incoming(osv.TransientModel):
         multiple documents to the workflow "archived" state.
         ---------------------------------------------------- '''
     def archive(self, cr, uid, ids, context=None):
-
         # Get the selected documents
         # --------------------------
         ids = context.get('active_ids',[])
         if not ids:
             raise osv.except_osv(_('Warning!'), _("You did not provide any documents to archive!"))
 
-
         # Push each document to archived
         # ------------------------------
         wf_service = netsvc.LocalService("workflow")
         for document in self.pool.get('clubit.tools.edi.document.incoming').browse(cr, uid, ids, context):
-            if document.state == 'processed':
+            if document.state in ['new','ready','processed','in_error']:
                 wf_service.trg_validate(uid, 'clubit.tools.edi.document.incoming', document.id, 'button_to_archived', cr)
 
+        return {'type': 'ir.actions.act_window_close'}
 
